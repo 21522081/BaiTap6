01615e530f415d594023c30556f8a0f4514d8454
LTH-21522081 <116651329+21522081@users.noreply.github.com>
2023-12-08 09:17:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ import numpy as np
 from PIL import Image
 
 class_list = {'0': 'Female', '1': 'Male'}
+st.title("Giới tính người Việt Nam dựa trên tên")
+
+input_ec = open('ec_vinames.pkl', 'rb')
+encoder = pkl.load(input_ec)
+
+input_md = open('lrc_vinames.pkl', 'rb')
+model = pkl.load(input_md)
+
+st.header('Write a feedback')
+txt = st.text_area('', '')
+
+if txt != '':
+  if st.button('Predict'):
+    feature_vector = encoder.transform([txt])
+    label = str((model.predict(feature_vector))[0])
+
+    st.header('Result')
+    st.text(class_list[label])
